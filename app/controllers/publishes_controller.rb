e71fabd48aa1f53b872f5dedcaa8b5be4f0f8835
Vic Ramon <vic@vicramon.com>
2014-02-22 20:31:38 -0500

    Require photos before publishing

--- app/controllers/publishes_controller.rb
+++ app/controllers/publishes_controller.rb
@@ -7,8 +7,13 @@ class PublishesController < EditTourController
     end
 
     if tour.no_live_path?
-      message = "You must enter a subdomain or custom domain before publishing your tour."
-      redirect_to tour_settings_path(tour), update: message and return
+      message = "You must enter a subdomain or custom domain before you can publish your tour."
+      redirect_to tour_settings_path(tour), error: message and return
+    end
+
+    if tour.photos.empty?
+      message = "You must upload photos before you can publish your tour."
+      redirect_to tour_photos_path(tour), error: message and return
     end
 
     render :already_paid and return if tour.paid?
