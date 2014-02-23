4aa5df582437b9b6cac6310dbc8573c44f92572a
Vic Ramon <vic@vicramon.com>
2014-02-22 20:15:20 -0500

    Heroku can't seem to find s3 keys while precompiling

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:s3_credentials] = {
   bucket: 'tour_files',
-  access_key_id: ENV.fetch('S3_KEY'),
-  secret_access_key: ENV.fetch('S3_SECRET_KEY')
+  access_key_id: ENV['S3_KEY'],
+  secret_access_key: ENV['S3_SECRET_KEY']
 }
