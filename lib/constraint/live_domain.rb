eeb76e0acc3e671c2afd71cc42f791b6840d32a6
Vic Ramon <vic@vicramon.com>
2014-03-01 02:06:17 -0500

    Don't redirect www.tourvibes.com

--- lib/constraint/live_domain.rb
+++ lib/constraint/live_domain.rb
@@ -3,7 +3,7 @@ module Constraint
     def matches?(request)
       return false if request.domain == 'herokuapp.com'
       !%w(tourvibes.com localhost tourvibes.dev).include? request.domain or
-        request.subdomain.present?
+        (request.subdomain.present? and request.subdomain != 'www')
     end
   end
 end
